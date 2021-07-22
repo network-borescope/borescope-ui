3c6bdef9b3a8a318c93ea11f3fd9075decb0123d
Marcos Lage <mlage@ic.uff.br>
2021-07-22 15:57:38 -0300

    Fix the tooltip

--- front/src/app/widgets/bar-chart/bar-chart.component.ts
+++ front/src/app/widgets/bar-chart/bar-chart.component.ts
@@ -18,11 +18,11 @@ export class BarChartComponent implements OnInit {
   @Output() checkboxClicked = new EventEmitter<number>();
 
   private barChart: any;
+
   private chartData: any = {};
   private colorList: any = [];
 
   private labels: any = [];
-  private nMarks: any = undefined;
 
   constructor(public global: GlobalService, public util: UtilService) { }
 
@@ -34,71 +34,84 @@ export class BarChartComponent implements OnInit {
  * Plota os dados do Mapa no grafico.
  */
   drawChart(responseData: any, dataId: string, chartColor: string) {
-    // cleans the data
-    //this.clearDataInfo(dataId);
-
     // calcula a soma dos valores em results
     const total = responseData.result.reduce((a: any, b: any) => {
       return a + b.v[0];
     }, 0);
 
-    if (!this.nMarks) {
-      this.nMarks = responseData.result.length;
-    }
-
     if(this.chartData[chartColor]) {
-      this.clearDataInfo(chartColor);
+      this.clearData(chartColor);
     };
-    
+
     // adiciona os valores normalizados ao dataset
     for (let i = 0; i < responseData.result.length; i++) {
-      const pointId = responseData.result[i].k[0];
+      const pointId = +responseData.result[i].k[0];
       const pointVl = responseData.result[i].v[0] / total;
 
-      this.addDataInfo(chartColor, pointId, pointVl);
+      this.addDataPoint(chartColor, pointId, pointVl);
     }
-    
+
     this.updateLabels(chartColor);
+    this.barChart.setLabels(this.labels);
+
+    this.fillMissingPoints();
     const data = this.getData(chartColor);
 
-    this.barChart.setLabels(this.labels);
     if(!this.colorList.includes(chartColor)) {
       this.barChart.addDataset(dataId, data, chartColor);
       this.colorList.push(chartColor);
-    } else {
+    }
+    else {
       this.barChart.updateDataset(chartColor, data);
     }
   };
 
-  clearDataInfo(color: string) {
-
+  clearData(color: string) {
     this.chartData[color] = [];
     this.barChart.removeDataset(color);
   }
 
   clearLabel(label: any, color: string) {
-  
     this.chartData[color] = [];
     this.barChart.removeLabel(label, color);
-    this.colorList.splice(this.colorList.indexOf(color),1); 
+    this.colorList.splice(this.colorList.indexOf(color),1);
   }
 
-  addDataInfo(color: string, markerId: any, value: number) {
+  addDataPoint(color: string, markerId: any, value: number) {
     if(!this.chartData[color]) {
       this.chartData[color] = [];
     }
 
     this.chartData[color].push({x: markerId, y: value});
+    console.log(color, markerId);
+  }
+
+  fillMissingPoints() {
+    const colors = Object.keys(this.chartData);
+
+    for (let color of colors) {
+      this.labels.forEach((label: number) => {
+          if (!this.chartData[color].some((d: any) => d.x === label)) {
+            this.chartData[color].push({x: label, y: 0});
+          }
+      });
+
+      this.chartData[color].sort((a: any, b: any) => a.x - b.x)
+    }
   }
 
   updateLabels(color: string) {
     this.chartData[color].forEach( (d: any) => {
-      const x = d['x'];
+      const x = +d['x'];
 
       if (!this.labels.includes(x)) {
         this.labels.push(x);
+
+        console.log(color, x);
       }
     });
+
+    this.labels.sort((a: number, b: number) => a - b);
   }
 
   getData(color: string) {
