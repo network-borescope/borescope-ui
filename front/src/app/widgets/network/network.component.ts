fc88e730fdfd95675c588cf551e41d93ec8d0c7e
Marcos Lage <mlage@ic.uff.br>
2021-10-01 11:44:57 -0300

    Código inicial da janela do gráfico de conexões.

---
+++ front/src/app/widgets/network/network.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GlobalService } from 'src/app/shared/global.service';
+import { UtilService } from 'src/app/shared/util.service';
+
+@Component({
+  selector: 'app-network',
+  templateUrl: './network.component.html',
+  styleUrls: ['./network.component.css']
+})
+export class NetworkComponent implements OnInit {
+
+  // referência para o div do grafico
+  @ViewChild("netChart", { static: true }) private netDiv!: ElementRef;
+
+  private netChart: any;
+
+  constructor(public global: GlobalService, public util: UtilService) { }
+
+  ngOnInit(): void {
+  }
+
+}
