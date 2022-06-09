cfba66cf923741b2fe436b4c9db55202fb38e111
Leonardo Ferreira <leovsferreira@gmail.com>
2022-06-09 20:38:24 -0300

    adding multiselect

--- front/src/app/functions-chart/functions-chart.component.ts
+++ front/src/app/functions-chart/functions-chart.component.ts
@@ -1,7 +1,9 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { GlobalService } from 'src/app/shared/global.service';
 import { UtilService } from 'src/app/shared/util.service';
+import { Functionschart } from './functions-chart';
 
 @Component({
   selector: 'app-functions-chart',
@@ -14,9 +16,33 @@ export class FunctionsChartComponent implements OnInit {
 
   @Output() functionsParamChanged = new EventEmitter<number>();
   @Output() functionsValueChanged = new EventEmitter<number>();
+  @Output() onItemSelected = new EventEmitter<any>();
+
   constructor(public global: GlobalService, public util: UtilService) { }
 
+  private selectedServices: any[] = [];
+  // objeto do gráfico
+  private functionsChart: any;
+  //capitals select list
+  private selectedItems: any = [];
+  public selectedItemsRoot: any = [];
+  //lista de cores
+  private drawColors: any = [
+    '#1F77B4', '#2CA02C', '#9467BD', '#8C564B', '#E377C2',
+    '#AEC7E8', '#98DF8A', '#C5B0D5', '#C49C94', '#F7B6D2'
+  ];
+  //lista de cores já usadas
+  private usedColors: any = [];
+  //multiselect
+  public dropdownList: any = [];
+  public dropdownSettings: any = {};
+  public multiSelectPlaceholder = 'Estados'
+
   ngOnInit(): void {
+    this.functionsChart = new Functionschart(this.functionsDiv.nativeElement);
+    this.setMultipleSelectConfiguration();
+    this.functionsChart.setCapitals(this.global.getGlobal('state_capitals').value.default);
+    this.functionsChart.setServices(this.global.getGlobal("services").value.default);
   }
 
 
@@ -33,13 +59,71 @@ export class FunctionsChartComponent implements OnInit {
   onValueChange(event: any) {
     const functions_value = {
       key: "functions_value",
-      value: event.target.value
+      pop: !this.isCapitalSelected() ? "" : this.global.getGlobal("clicked_element").value,
+      services: this.selectedServices
     };
     this.global.setGlobal(functions_value);
     this.functionsValueChanged.emit();
   }
 
+  onItemSelect(event: any, added: boolean) {
+    if(this.selectedItems.includes(event.cod)) {
+      const index = this.selectedItems.indexOf(event.cod);
+      this.selectedItems = this.selectedItems.filter((e:any) => e !== event.cod);
+      this.updateUsedColors(false, this.usedColors[index]);
+    } else {
+      this.selectedItems.push(event.cod);
+    };
+    this.onItemSelected.emit(this.selectedItems);
+  }
+  
+  setMultipleSelectConfiguration() {
+    //multiselect
+    this.dropdownList = [];
+    this.dropdownSettings = {};
+    //setando as configuracoes do multiselect
+    const services = this.global.getGlobal("services").value.default;
+    this.multiSelectPlaceholder = 'Serviços';
+    for(let i = 0; i < services.length; i++) {
+      let id = services[i].id.toUpperCase();
+      let cod = services[i].cod;
+      let obj:any = {};
+      obj['estado'] = id;
+      obj['cod'] = cod;
+      this.dropdownList.push(obj);
+    };  
+    this.dropdownSettings = {
+      singleSelection: false,
+      limitSelection: 10,
+      idField: 'cod',
+      textField: 'estado',
+      enableCheckAll: false,
+      unSelectAll: false,
+      itemsShowLimit: 0,
+      allowSearchFilter: false
+    };    
+  }
+    
   isCapitalSelected() {
     return (this.global.getGlobal("clicked_element").value > 0)
   }
+
+  clearSeries() {
+    this.selectedItems = [];
+    this.selectedItemsRoot = [];
+    this.functionsChart.clear();
+  }
+
+  /**
+ * seta as cores já utilizadas
+ */
+  updateUsedColors(added: boolean, color: string) {
+    if (added) {
+      if(!this.usedColors.includes(color)) this.usedColors.push(color);
+    }
+    else {
+      this.usedColors = this.usedColors.filter( (d: string) => d !== color );
+    }
+  }
+
 }
---
+++ front/src/app/functions-chart/functions-chart.ts
@@ -0,0 +1,142 @@
+import { CategoryScale, Chart, Legend, LinearScale, LineController, LineElement, PointElement, Tooltip } from 'chart.js';
+
+export class Functionschart {
+
+    private chart: any;
+    private canvas: HTMLCanvasElement;
+    // capitals
+    private capitals: any = null;
+    // services
+    private services: any = null;
+    // labels 
+    private labels: any = null;
+    private data: any = null;
+    
+    constructor(canvas: HTMLCanvasElement) {
+      this.canvas = canvas;
+      this.init();
+    }
+  
+    //Configuração do grafico
+    init() {
+      if (this.canvas === undefined) {
+        return;
+      }
+  
+      //Registra os elementos utilizados pelo grafico
+      Chart.register(PointElement, LineElement, LineController, CategoryScale, LinearScale, Legend, Tooltip);
+      Chart.defaults.animation = false;
+  
+      this.chart = new Chart(this.canvas, {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: []
+        },
+        options: {
+          plugins: {
+            legend: {
+              title: {
+                display: true,
+                text: 'Legend'
+              },
+              display: true,
+              position: "top",
+            },
+            title: {
+              display: false,
+              text: 'Title'
+            },
+            tooltip: {
+              mode: 'index',
+              intersect: false,
+            },
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          },
+          responsive: true,
+          hover: {
+            mode: 'nearest',
+            intersect: true
+          },
+          scales: {
+            x: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Tempo (ms)'
+              }
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'F(X)'
+              }
+            }
+          }
+        }
+      });
+    }
+  
+  
+    //Modifica as configurações globais para os títulos
+    setTitle(id: number) {
+      this.chart.options.plugins.legend.title.text = this.getId(id, 'pop');
+    }
+  
+    setCapitals(capitals: any) {
+      this.capitals = capitals;
+    }
+  
+    setServices(services: any) {
+      this.services = services;
+    }
+  
+    updateData(data: any, colorList: any, type: string) {
+      const datasets = this.chart.config.data.datasets;
+      for(let i = 0; i < data.length; i++) {
+        const newData = {
+          label: this.getId(data[i][0], type),
+          data: data[i][1],
+          backgroundColor: colorList[i],
+          borderColor: colorList[i],
+          fill: false
+        };
+        datasets.push(newData);
+      }
+      this.chart.update();
+    }
+  
+    setLabels(labels: any, type: string) {
+        this.chart.config.data.labels = labels;
+    }
+  
+    getId(id: number, type: string) {
+      if(type == 'pop') {
+        return this.capitals.filter((c: any) => c.cod === id)[0].id.toUpperCase();
+      } else {
+        return this.services.filter((c: any) => c.cod === id)[0].id.toUpperCase();
+      }
+    }
+  
+    clear() {
+      this.chart.data.labels = [];
+      this.chart.data.datasets = [];
+      this.chart.config.options.scales.y.title.text = '';
+      this.chart.update();
+    }
+  
+    render() {
+      this.chart.update();
+    }
+  
+    removeDataset() {
+      this.chart.config.data.labels = [];
+      this.chart.config.data.datasets = [];
+      this.chart.update();
+    }
+  }
